008e276a06f8791af44c7d0902e8fb6ba4d8d3b4
Loki_79 <76516343+loki79uk@users.noreply.github.com>
2024-11-15 16:07:14 +0000

    First working version

---
+++ register.lua
@@ -0,0 +1,16 @@
+-- ============================================================= --
+-- Cabin View MOD
+-- ============================================================= --
+CabViewREGISTER = {}
+
+g_specializationManager:addSpecialization('cabView', 'CabView', Utils.getFilename('CabView.lua', g_currentModDirectory), "")
+
+for vehicleName, vehicleType in pairs(g_vehicleTypeManager.types) do
+	if  SpecializationUtil.hasSpecialization(Drivable,  vehicleType.specializations) and
+		SpecializationUtil.hasSpecialization(Motorized, vehicleType.specializations) and
+		SpecializationUtil.hasSpecialization(Enterable, vehicleType.specializations) and
+		SpecializationUtil.hasSpecialization(Dashboard, vehicleType.specializations) then
+			g_vehicleTypeManager:addSpecialization(vehicleName, g_currentModName .. '.cabView') 	
+		--print("INSTALLED: "..vehicleName)
+	end
+end
